b0a11c434b1d2e959b373b2cadbb02499e02465d
azzyy <azzyypaaras@azzyys.works>
2025-05-10 20:58:17 -0500

    Fix mixins

--- src/main/java/de/dafuqs/arrowhead/mixin/BowItemMixin.java
+++ src/main/java/de/dafuqs/arrowhead/mixin/BowItemMixin.java
@@ -11,29 +11,16 @@ import org.spongepowered.asm.mixin.injection.callback.*;
 
 @Mixin(BowItem.class)
 public class BowItemMixin {
-	
-	@ModifyVariable(method = "shoot(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/entity/projectile/ProjectileEntity;IFFFLnet/minecraft/entity/LivingEntity;)V", at = @At("HEAD"), ordinal = 0, argsOnly = true)
-	public float arrowhead$handleBowSpeed(float originalSpeed, LivingEntity shooter, ProjectileEntity projectile, int index, float speed, float divergence, float yaw, @Nullable LivingEntity target) {
+
+	@Inject(method = "shoot(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/entity/projectile/ProjectileEntity;IFFFLnet/minecraft/entity/LivingEntity;)V", at = @At(value = "HEAD"))
+	public void arrowhead$handleRangedWeapon(LivingEntity shooter, ProjectileEntity projectile, int index, float speed, float divergence, float yaw, LivingEntity target, CallbackInfo ci) {
 		ItemStack activeStack = shooter.getActiveItem();
+
 		if (activeStack.getItem() instanceof ArrowheadBow arrowheadBow) {
-			originalSpeed *= arrowheadBow.getProjectileVelocityModifier(activeStack);
+			projectile.setVelocity(shooter, shooter.getPitch(), shooter.getYaw() + yaw, 0.0F, speed * arrowheadBow.getProjectileVelocityModifier(activeStack), divergence * arrowheadBow.getDivergenceMod(activeStack));
 		}
-		return originalSpeed;
-	}
-	
-	@ModifyVariable(method = "shoot(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/entity/projectile/ProjectileEntity;IFFFLnet/minecraft/entity/LivingEntity;)V", at = @At("HEAD"), ordinal = 1, argsOnly = true)
-	public float arrowhead$handleBowDivergence(float originalDivergence, LivingEntity shooter, ProjectileEntity projectile, int index, float speed, float divergence, float yaw, @Nullable LivingEntity target) {
-		ItemStack activeStack = shooter.getActiveItem();
-		if (activeStack.getItem() instanceof ArrowheadBow arrowheadBow) {
-			originalDivergence *= arrowheadBow.getDivergenceMod(activeStack);
-		}
-		return originalDivergence;
-	}
-	
-	@Inject(method = "shoot(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/entity/projectile/ProjectileEntity;IFFFLnet/minecraft/entity/LivingEntity;)V", at = @At(value = "TAIL"))
-	public void arrowhead$bowCallbacks(LivingEntity shooter, ProjectileEntity projectile, int index, float speed, float divergence, float yaw, LivingEntity target, CallbackInfo ci) {
-		ItemStack activeStack = shooter.getActiveItem();
-		for (BowShootingCallback callback : BowShootingCallback.callbacks) {
+
+		for(BowShootingCallback callback : BowShootingCallback.callbacks) {
 			callback.trigger(shooter.getWorld(), shooter, activeStack, activeStack.getMaxUseTime(shooter) - shooter.getItemUseTimeLeft(), projectile);
 		}
 	}
